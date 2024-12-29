190214e8e1ff13edbbfcff78a8ec3b35121db265
Jeremiah <jeremiahjacob261@gmail.com>
2024-12-29 07:08:35 +0100

    improve 2

--- src/main/java/com/fuoye/quiz/fuoyequiz/CourseController.java
+++ src/main/java/com/fuoye/quiz/fuoyequiz/CourseController.java
@@ -3,8 +3,14 @@ package com.fuoye.quiz.fuoyequiz;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
@@ -17,6 +23,9 @@ public class CourseController implements Initializable {
 
     @FXML
     private ListView<String> lists;
+
+    @FXML
+    private Label selected;
 
     String selectedCourse;
     String[] courses = {"CSC 101: Introduction to Computing", "BIO 101: Into to Plant Science Biology","PHY 101: Introduction to Physics", "GST 103: English"};
@@ -33,7 +42,17 @@ public class CourseController implements Initializable {
             @Override
             public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
                 selectedCourse = lists.getSelectionModel().getSelectedItem();
+                selected.setText("Selected Course:" + selectedCourse);
             }
         });
+    }
+
+    @FXML
+    public void attempt(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("question.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ src/main/java/com/fuoye/quiz/fuoyequiz/QuestionController.java
@@ -0,0 +1,5 @@
+package com.fuoye.quiz.fuoyequiz;
+
+public class QuestionController {
+
+}
